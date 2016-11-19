51c4f6b96dbdcb78d7a98c34f62b053d4d6a210d
Martin Jakubik <martin@jakubik.info>
2016-11-19 19:50:10 +0100

    Init MPI and query rank and cluster size

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <iostream>
 #include <omp.h>
+#include <mpi.h>
 
 #include "Window.hpp"
 #include "Input.hpp"
@@ -10,18 +11,27 @@
 #include "Raytracer.hpp"
 
 
-int main() {
-    auto window = Window::Create(500, 400, "Raytracer - OpenMP / MPI");
-    Shaders::CreateAndLoad();
-    Texture::Create();
-    auto quad = FullscreenQuad::Create();
-    auto raytracer = Raytracer::Create();
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int machineCount;
+    int rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &machineCount);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::cout << "Number of machines in the cluster: " << machineCount << std::endl;
+    std::cout << "This machine's rank: " << rank << std::endl;
 
     #pragma omp parallel
     #pragma omp master
     {
         std::cout << "Number of threads: " << omp_get_num_threads() << std::endl;
     }
+
+    auto window = Window::Create(500, 400, "Raytracer - OpenMP / MPI");
+    Shaders::CreateAndLoad();
+    Texture::Create();
+    auto quad = FullscreenQuad::Create();
+    auto raytracer = Raytracer::Create();
 
     auto start = std::chrono::high_resolution_clock::now();
     auto last = std::chrono::high_resolution_clock::now();
@@ -46,6 +56,8 @@ int main() {
         last = timestamp;
         std::cout << delta << "ms\n";
     }
+
+    MPI_Finalize();
 
     return 0;
 }
